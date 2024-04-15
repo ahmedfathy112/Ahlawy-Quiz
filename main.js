d830d51fe305832695e32e98cd0962f4c3b7c407
Ahmed Fathi <98648216+ahmedfathy112@users.noreply.github.com>
2024-04-16 00:37:04 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,393 @@
+// let quickSec1 = document.getElementById("quik1");
+// let quickSec2 = document.getElementById("quik2");
+// let quickSec3 = document.getElementById("quik3");
+// let quickSec4 = document.getElementById("quik4");
+// let quickSec5 = document.getElementById("quik5");
+// let answer1Quik1 = document.getElementById("qui1answer1");
+// let answer2Quik1 = document.getElementById("qui1answer2");
+// let answer3Quik1 = document.getElementById("qui1answer3");
+// let answer4Quik1 = document.getElementById("qui1answer4");
+// let next1 = document.getElementById("qui1");
+// let next2 = document.getElementById("qui2");
+// let next3 = document.getElementById("qui3");
+// let next4 = document.getElementById("qui4");
+// let next5 = document.getElementById("qui5");
+// let answer1Quik2 = document.getElementById("qui2answer1");
+// let answer2Quik2 = document.getElementById("qui2answer2");
+// let answer3Quik2 = document.getElementById("qui2answer3");
+// let answer4Quik2 = document.getElementById("qui2answer4");
+
+// let answer1Quik3 = document.getElementById("qui3answer1");
+// let answer2Quik3 = document.getElementById("qui3answer2");
+// let answer3Quik3 = document.getElementById("qui3answer3");
+// let answer4Quik3 = document.getElementById("qui3answer4");
+
+// let answer1Quik4 = document.getElementById("qui4answer1");
+// let answer2Quik4 = document.getElementById("qui4answer2");
+// let answer3Quik4 = document.getElementById("qui4answer3");
+// let answer4Quik4 = document.getElementById("qui4answer4");
+
+// let answer1Quik5 = document.getElementById("qui5answer1");
+// let answer2Quik5 = document.getElementById("qui5answer2");
+// let answer3Quik5 = document.getElementById("qui5answer3");
+// let answer4Quik5 = document.getElementById("qui5answer4");
+// // دا السؤال الاول
+// next1.disabled = true;
+// answer1Quik1.addEventListener("click", () => {
+//   answer1Quik1.style.backgroundColor = "red";
+//   answer1Quik1.style.color = "#fff";
+//   answer4Quik1.disabled = true;
+//   answer2Quik1.disabled = true;
+//   answer3Quik1.disabled = true;
+//   next1.disabled = false;
+//   alert("للاسف اجابتك غلط...حد يغلط في دي بردو!!");
+// });
+// answer2Quik1.addEventListener("click", () => {
+//   answer2Quik1.style.backgroundColor = "green";
+//   answer2Quik1.style.color = "#fff";
+//   answer1Quik1.disabled = true;
+//   answer4Quik1.disabled = true;
+//   answer3Quik1.disabled = true;
+//   next1.disabled = false;
+//   alert("اجابه صح يبن اللاعيبه😉😉");
+// });
+// answer3Quik1.addEventListener("click", () => {
+//   answer3Quik1.style.backgroundColor = "red";
+//   answer3Quik1.style.color = "#fff";
+//   answer1Quik1.disabled = true;
+//   answer2Quik1.disabled = true;
+//   answer4Quik1.disabled = true;
+//   next1.disabled = false;
+//   alert("للاسف اجابتك غلط...حد يغلط في دي بردو!!");
+// });
+// answer4Quik1.addEventListener("click", () => {
+//   answer4Quik1.style.backgroundColor = "red";
+//   answer4Quik1.style.color = "#fff";
+//   answer1Quik1.disabled = true;
+//   answer2Quik1.disabled = true;
+//   answer3Quik1.disabled = true;
+//   next1.disabled = false;
+//   alert("للاسف اجابتك غلط...حد يغلط في دي بردو!!");
+// });
+// if ((next1.disabled = false)) {
+//   quickSec2.style.display = "block";
+//   quickSec1.style.display = "block";
+// }
+// next1.addEventListener("click", () => {
+//   if ((next1.disabled = true)) {
+//     quickSec2.style.display = "block";
+//     quickSec1.style.display = "none";
+//   } else {
+//     alert("اختار اجابه الاول يصاحبي!!");
+//   }
+// });
+
+// // دا السؤال التاني
+// next2.disabled = true;
+// answer1Quik2.addEventListener("click", () => {
+//   answer1Quik2.style.backgroundColor = "red";
+//   answer1Quik2.style.color = "#fff";
+//   answer4Quik2.disabled = true;
+//   answer2Quik2.disabled = true;
+//   answer3Quik2.disabled = true;
+//   next2.disabled = false;
+//   alert("للاسف اجابتك غلط...حد يغلط في دي بردو!!");
+// });
+// answer2Quik2.addEventListener("click", () => {
+//   answer2Quik2.style.backgroundColor = "red";
+//   answer2Quik2.style.color = "#fff";
+//   answer1Quik2.disabled = true;
+//   answer4Quik2.disabled = true;
+//   answer3Quik2.disabled = true;
+//   next2.disabled = false;
+//   alert("للاسف اجابتك غلط...حد يغلط في دي بردو!!");
+// });
+// answer3Quik2.addEventListener("click", () => {
+//   answer3Quik2.style.backgroundColor = "red";
+//   answer3Quik2.style.color = "#fff";
+//   answer1Quik2.disabled = true;
+//   answer2Quik2.disabled = true;
+//   answer4Quik2.disabled = true;
+//   next2.disabled = false;
+//   alert("للاسف اجابتك غلط...حد يغلط في دي بردو!!");
+// });
+// answer4Quik2.addEventListener("click", () => {
+//   answer4Quik2.style.backgroundColor = "green";
+//   answer4Quik2.style.color = "#fff";
+//   answer1Quik2.disabled = true;
+//   answer2Quik2.disabled = true;
+//   answer3Quik2.disabled = true;
+//   next2.disabled = false;
+//   alert("اجابه صح يبن اللاعيبه😉😉");
+// });
+// next2.addEventListener("click", () => {
+//   if ((next1.disabled = true)) {
+//     quickSec1.style.display = "none";
+//     quickSec2.style.display = "none";
+//     quickSec3.style.display = "block";
+//   } else {
+//     alert("اختار اجابه الاول يصاحبي!!");
+//   }
+// });
+
+// // السؤال التالت
+
+// next3.disabled = true;
+// answer1Quik3.addEventListener("click", () => {
+//   answer1Quik3.style.backgroundColor = "green";
+//   answer1Quik3.style.color = "#fff";
+//   answer4Quik3.disabled = true;
+//   answer2Quik3.disabled = true;
+//   answer3Quik3.disabled = true;
+//   next3.disabled = false;
+//   alert("اجابه صح يبن اللاعيبه😉😉");
+// });
+// answer2Quik3.addEventListener("click", () => {
+//   answer2Quik3.style.backgroundColor = "green";
+//   answer2Quik3.style.color = "#fff";
+//   answer1Quik3.disabled = true;
+//   answer4Quik3.disabled = true;
+//   answer3Quik3.disabled = true;
+//   next3.disabled = false;
+//   alert("للاسف اجابتك غلط...حد يغلط في دي بردو!!");
+// });
+// answer3Quik3.addEventListener("click", () => {
+//   answer3Quik3.style.backgroundColor = "red";
+//   answer3Quik3.style.color = "#fff";
+//   answer1Quik3.disabled = true;
+//   answer2Quik3.disabled = true;
+//   answer4Quik3.disabled = true;
+//   next3.disabled = false;
+//   alert("للاسف اجابتك غلط...حد يغلط في دي بردو!!");
+// });
+// answer4Quik3.addEventListener("click", () => {
+//   answer4Quik3.style.backgroundColor = "red";
+//   answer4Quik3.style.color = "#fff";
+//   answer1Quik3.disabled = true;
+//   answer2Quik3.disabled = true;
+//   answer3Quik3.disabled = true;
+//   next3.disabled = false;
+//   alert("للاسف اجابتك غلط...حد يغلط في دي بردو!!");
+// });
+// next3.addEventListener("click", () => {
+//   if ((next3.disabled = true)) {
+//     quickSec1.style.display = "none";
+//     quickSec2.style.display = "none";
+//     quickSec3.style.display = "none";
+//     quickSec4.style.display = "block";
+//   } else {
+//     alert("اختار اجابه الاول يصاحبي!!");
+//   }
+// });
+
+// // السؤال الرابع
+
+// next4.disabled = true;
+// answer1Quik4.addEventListener("click", () => {
+//   answer1Quik4.style.backgroundColor = "red";
+//   answer1Quik4.style.color = "#fff";
+//   answer4Quik4.disabled = true;
+//   answer2Quik4.disabled = true;
+//   answer3Quik4.disabled = true;
+//   next4.disabled = false;
+//   alert("للاسف اجابتك غلط...حد يغلط في دي بردو!!");
+// });
+// answer2Quik4.addEventListener("click", () => {
+//   answer2Quik4.style.backgroundColor = "red";
+//   answer2Quik4.style.color = "#fff";
+//   answer1Quik4.disabled = true;
+//   answer4Quik4.disabled = true;
+//   answer3Quik4.disabled = true;
+//   next4.disabled = false;
+//   alert("للاسف اجابتك غلط...حد يغلط في دي بردو!!");
+// });
+// answer3Quik4.addEventListener("click", () => {
+//   answer3Quik4.style.backgroundColor = "red";
+//   answer3Quik4.style.color = "#fff";
+//   answer1Quik4.disabled = true;
+//   answer2Quik4.disabled = true;
+//   answer4Quik4.disabled = true;
+//   next4.disabled = false;
+//   alert("للاسف اجابتك غلط...حد يغلط في دي بردو!!");
+// });
+// answer4Quik3.addEventListener("click", () => {
+//   answer4Quik4.style.backgroundColor = "green";
+//   answer4Quik4.style.color = "#fff";
+//   answer1Quik4.disabled = true;
+//   answer2Quik4.disabled = true;
+//   answer3Quik4.disabled = true;
+//   next4.disabled = false;
+//   alert("اجابه صح يبن اللاعيبه😉😉");
+// });
+// next4.addEventListener("click", () => {
+//   if ((next4.disabled = true)) {
+//     quickSec1.style.display = "none";
+//     quickSec2.style.display = "none";
+//     quickSec3.style.display = "none";
+//     quickSec4.style.display = "block";
+//   } else {
+//     alert("اختار اجابه الاول يصاحبي!!");
+//   }
+// });
+// ----------------------------------------------------------------------
+// let currentQuestion = 0;
+// let questions = document.querySelectorAll(".question");
+let answeredCorrectly = false;
+
+// function checkAnswer1(answer, button) {
+//   if (answer === "رامي ربيعه") {
+//     alert("اجابه صح يبن اللاعيبه😉😉");
+//     answeredCorrectly = true;
+//     button.classList.add("correct");
+//   } else {
+//     alert("للاسف اجابتك غلط...حد يغلط في دي بردو!!");
+//     answeredCorrectly = true;
+//     button.classList.add("wrong");
+//   }
+//   document
+//     .querySelectorAll(".question")
+//     [currentQuestion].querySelectorAll("button")
+//     .forEach(function (btn) {
+//       btn.disabled = true;
+//     });
+//   document.getElementById("nextButton").disabled = false; // enable the next button after answering
+// }
+
+// function checkAnswer2(answer) {
+//   if (answer === "حسام غالي") {
+//     alert("اجابه صح يبن اللاعيبه😉😉");
+//     answeredCorrectly = true;
+//     button.classList.add("correct");
+//   } else {
+//     alert("للاسف اجابتك غلط...حد يغلط في دي بردو!!");
+//     answeredCorrectly = true;
+//     button.classList.add("wrong");
+//   }
+//   document.getElementById("nextButton").disabled = false; // enable the next button after answering
+// }
+// function checkAnswer3(answer) {
+//   if (answer === "ابوتريكه") {
+//     alert("اجابه صح يبن اللاعيبه😉😉");
+//     answeredCorrectly = true;
+//     button.classList.add("correct");
+//   } else {
+//     alert("للاسف اجابتك غلط...حد يغلط في دي بردو!!");
+//     answeredCorrectly = true;
+//     button.classList.add("wrong");
+//   }
+//   document.getElementById("nextButton").disabled = false; // enable the next button after answering
+// }
+// function checkAnswer4(answer) {
+//   if (answer === "بيرسي تاو") {
+//     alert("اجابه صح يبن اللاعيبه😉😉");
+//     answeredCorrectly = true;
+//     button.classList.add("correct");
+//   } else {
+//     alert("للاسف اجابتك غلط...حد يغلط في دي بردو!!");
+//     answeredCorrectly = true;
+//     button.classList.add("wrong");
+//   }
+
+//   document.getElementById("nextButton").disabled = false; // enable the next button after answering
+// }
+// function checkAnswer5(answer) {
+//   if (answer === "22") {
+//     alert("اجابه صح يبن اللاعيبه😉😉");
+//     answeredCorrectly = true;
+//     button.classList.add("correct");
+//   } else {
+//     alert("للاسف اجابتك غلط...حد يغلط في دي بردو!!");
+//     answeredCorrectly = true;
+//     button.classList.add("wrong");
+//   }
+//   document.getElementById("nextButton").disabled = false;
+// }
+// function moveToNextQuestion() {
+//   if (
+//     currentQuestion > 0 &&
+//     !questions[currentQuestion - 1].querySelector("button.correct")
+//   ) {
+//     alert("متجاوب الاول يصحبي😒");
+//     return;
+//   }
+
+//   questions[currentQuestion].style.display = "none";
+//   currentQuestion++;
+//   if (currentQuestion < questions.length) {
+//     questions[currentQuestion].style.display = "block";
+//   } else {
+//     alert("Quiz is finished!");
+//   }
+//   document.getElementById("nextButton").disabled = true;
+// }
+
+// questions[currentQuestion].style.display = "block";
+// -----------------------------------------------------------------------------------
+let currentQuestion = 0;
+let questions = document.querySelectorAll(".question");
+let correctAnswersCount = 0; // متغير يحمل عدد الأسئلة الصحيحة
+
+function checkAnswer(answer, button) {
+  // تحديد ما إذا كانت الإجابة صحيحة وزيادة عدد الإجابات الصحيحة إذا كانت الإجابة صحيحة
+  if (answer === "رامي ربيعه" && button.textContent.includes("رامي ربيعه")) {
+    button.classList.add("correct");
+    alert("اجابه صح يبن اللاعيبه😉😉");
+    correctAnswersCount++;
+  } else if (answer === "22") {
+    // button.classList.add("correct");
+    alert("اجابه صح يبن اللاعيبه😉😉");
+    correctAnswersCount++;
+  } else if (answer === "ابوتريكه" && button.textContent.includes("ابوتريكه")) {
+    button.classList.add("correct");
+    alert("اجابه صح يبن اللاعيبه😉😉");
+    correctAnswersCount++;
+  } else if (
+    answer === "بيرسي تاو" &&
+    button.textContent.includes("بيرسي تاو")
+  ) {
+    button.classList.add("correct");
+    alert("اجابه صح يبن اللاعيبه😉😉");
+    correctAnswersCount++;
+  } else if (
+    answer === "حسام غالي" &&
+    button.textContent.includes("حسام غالي")
+  ) {
+    button.classList.add("correct");
+    alert("اجابه صح يبن اللاعيبه😉😉");
+    correctAnswersCount++;
+  } else {
+    button.classList.add("wrong");
+    alert("للاسف اجابتك غلط...حد يغلط في دي بردو!!");
+  }
+  // تحديث نص العنصر الذي يحمل الفئة .score بقيمة عدد الإجابات الصحيحة
+  document.getElementById("scoreValue").textContent = correctAnswersCount;
+  // تعطيل جميع الأزرار بعد الإجابة
+  document
+    .querySelectorAll(".question")
+    [currentQuestion].querySelectorAll(".answer")
+    .forEach(function (btn) {
+      btn.disabled = true;
+    });
+}
+
+function moveToNextQuestion() {
+  if (currentQuestion >= questions.length) {
+    alert("Quiz is finished!");
+    return;
+  }
+  if (
+    currentQuestion > 0 &&
+    !questions[currentQuestion - 1].querySelector("button.correct")
+  ) {
+    alert("You have not answered the previous question!");
+    return;
+  }
+  questions[currentQuestion].style.display = "none";
+  currentQuestion++;
+  if (currentQuestion < questions.length) {
+    questions[currentQuestion].style.display = "block";
+  }
+}
+
+// Show the first question initially
+questions[currentQuestion].style.display = "block";
